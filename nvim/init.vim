6313fe691dc8cb5a123d8e601f931446edab0148
Chentse Lee <chentselee@gmail.com>
2022-08-21 02:06:46 +0800

    reorganize plugins

--- nvim/init.vim
+++ nvim/init.vim
@@ -4,7 +4,4 @@ lua require("plugins")
 lua require("treesitter")
 lua require("lsp")
 lua require("theme")
-lua require("telescope")
-lua require("camelcasemotion")
-lua require("emmet")
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,7 +2,8 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local packer_bootstrap = false
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+    install_path })
 end
 
 return require("packer").startup(function(use)
@@ -12,13 +13,11 @@ return require("packer").startup(function(use)
 
   use 'williamboman/nvim-lsp-installer'
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'onsails/lspkind-nvim'
   use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
 
   use 'Mofiqul/vscode.nvim'
 
