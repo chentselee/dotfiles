eaf5cbb1b78655b7e15b6a36e1a70d91fb8a0517
Chentse Lee <chentselee@gmail.com>
2022-08-24 20:57:34 +0800

    add lspsaga

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,6 +18,7 @@ return require("packer").startup(function(use)
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/nvim-cmp'
+  use 'glepnir/lspsaga.nvim'
 
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
