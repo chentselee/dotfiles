43faf0d9bd7e1a40a27e7ef2b6d2a54c95292f84
Chentse Lee <chentselee@gmail.com>
2022-11-10 22:31:10 +0800

    switch plugins to stable branch

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,25 +8,69 @@ end
 
 return require("packer").startup(function(use)
   use 'wbthomason/packer.nvim'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'master'
+  }
+  use {
+    'williamboman/mason.nvim',
+    branch = 'main'
+  }
+  use {
+    'williamboman/mason-lspconfig.nvim',
+    branch = 'main'
+  }
+  use {
+    'neovim/nvim-lspconfig',
+    branch = 'master'
+  }
+  use {
+    'L3MON4D3/LuaSnip',
+    branch = 'master'
+  }
+  use {
+    'onsails/lspkind-nvim',
+    branch = 'master'
+  }
+  use {
+    'hrsh7th/cmp-buffer',
+    branch = 'main'
+  }
+  use {
+    'hrsh7th/cmp-nvim-lsp',
+    branch = 'main'
+  }
+  use {
+    'hrsh7th/nvim-cmp',
+    branch = 'main'
+  }
+  use {
+    'glepnir/lspsaga.nvim',
+    branch = 'main'
+  }
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    branch = 'main'
+  }
+  use {
+    'MunifTanjim/prettier.nvim',
+    branch = 'main'
+  }
 
-  use 'nvim-treesitter/nvim-treesitter'
+  use {
+    'windwp/nvim-autopairs',
+    branch = 'master'
+  }
+  use {
+    'windwp/nvim-ts-autotag',
+    branch = 'main'
+  }
 
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'L3MON4D3/LuaSnip'
-  use 'onsails/lspkind-nvim'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/nvim-cmp'
-  use 'glepnir/lspsaga.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'MunifTanjim/prettier.nvim'
-
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-
-  use 'Mofiqul/vscode.nvim'
+  use {
+    'Mofiqul/vscode.nvim',
+    -- branch = 'main'
+    commit = "c5125820a0915ef50f03fae10423c43dc49c66b1"
+  }
 
   use({
     "glepnir/galaxyline.nvim",
