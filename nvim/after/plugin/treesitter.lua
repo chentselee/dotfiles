0617a3aa71ccadbd42a26730b90947e809c7f6c9
cz.lee <cz.lee@chelpis.com>
2023-05-18 16:28:02 +0800

    ignore install phpdoc to avoid arch error

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require "nvim-treesitter.configs".setup {
   -- ensure_installed can be "all" or a list of languages { "python", "javascript" }
   ensure_installed = "all",
-  ignore_install = { "haskell" },
+  ignore_install = { "haskell", "phpdoc" },
   highlight = {
     -- enable highlighting for all file types
     enable = true -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
