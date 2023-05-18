40030b834fa1f5b8584979d6ce849c6948d1feaf
cz.lee <cz.lee@chelpis.com>
2023-05-18 16:24:11 +0800

    migrate prettier to null_ls

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -2,11 +2,30 @@ local status, null_ls = pcall(require, 'null-ls')
 
 if (not status) then return end
 
+local formatting = null_ls.builtins.formatting
+
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.code_actions.eslint_d
   }
+  sources = {
+    formatting.eslint_d,
+    formatting.prettierd.with({
+      filetypes = {
+        'html',
+        'css',
+        'scss',
+        'less',
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+        'json',
+        'vue'
+      }
+    }),
+  },
   on_attach = function(client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
       vim.cmd([[
