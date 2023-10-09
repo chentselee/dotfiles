db4748e81197ef517a0ee6695d596280255d6890
Chentse Lee <chentselee@gmail.com>
2023-10-09 10:21:52 +0800

    change harpoon maps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,7 +5,7 @@ if (not status) then return end
 
 vim.keymap.set("n", "<Leader>ha", mark.add_file)
 vim.keymap.set("n", "<Leader>e", ui.toggle_quick_menu)
-vim.keymap.set("n", "<Leader>h1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<Leader>h2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<Leader>h3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<Leader>h4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<Leader>1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<Leader>2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<Leader>3", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<Leader>4", function() ui.nav_file(4) end)
