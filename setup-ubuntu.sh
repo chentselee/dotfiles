4ea6bb09a708674953180637b326e38425d23ea7
Chentse Lee <chenstselee@gmail.com>
2024-10-15 16:53:28 +0800

    add docker

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo apt update
-sudo apt install -y curl git zsh alacritty cmus
+sudo apt install -y curl git zsh ca-certificates alacritty cmus
 
 git clone git@github.com:chentselee/dotfiles.git
 
@@ -37,6 +37,19 @@ unzip SourceCodePro.zip -d ~/.local/share/fonts/SourceCodePro
 fc-cache -fv ~/.local/share/fonts
 
 # development
+
+# docker
+# See https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
 
 # nvm
 PROFILE=/dev/null curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
